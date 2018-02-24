93adf09ba468f914e0965a61e95899bb826a8003
zxt_z <zxt_zel_npl@163.com>
2018-02-25 00:24:37 +0800

    add test for vars

---
+++ zxt/src/DemoOne.java
@@ -0,0 +1,15 @@
+public class DemoOne {
+    static int num1 = 0;
+    {
+        System.out.println("构造方法上面");
+    }
+    static {
+        System.out.println("静态语句块");
+    }
+    public DemoOne(){
+        System.out.println("构造方法");
+    }
+    public static void test(){
+        System.out.println("'test'");
+    }
+}
--- zxt/src/Main.java
+++ zxt/src/Main.java
@@ -3,5 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] arts){
         System.out.println("My Main");
+//        DemoOne demo = new DemoOne();
+//        DemoOne demo1 = new DemoOne();
+        DemoOne.test();
     }
 }
