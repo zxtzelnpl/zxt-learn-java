145a351f4dd5b5d5940b64428e7e61605c471240
was <zxt_zel_npl@163.com>
2018-02-27 17:58:17 +0800

    add Main of ZXT

---
+++ zxt/src/DemoThree.java
@@ -0,0 +1,23 @@
+public class DemoThree extends Thread {
+    private Thread t;
+    private String threadName;
+
+    DemoThree(String name){
+        threadName = name;
+        System.out.println("Creating "+threadName);
+    }
+
+    public void run(){
+        System.out.println("Running "+threadName);
+        try{
+            for(int i = 4;i>0;i--){
+                System.out.println("Thread: "+threadName + ", "+i);
+                Thread.sleep(50);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread "+threadName+" interrupted.");
+        }
+        System.out.println("Thread "+threadName + " exiting.");
+    }
+}
--- zxt/src/Main.java
+++ zxt/src/Main.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] arts){
         System.out.println("My Main");
-        DemoOne demo = new DemoOne();
-//        DemoOne demo1 = new DemoOne();
-        DemoOne.test();
-        demo.testForObj();
+        DemoThree d1 = new DemoThree("Thread-1");
+        d1.start();
+
+        DemoThree d2 = new DemoThree("Thread-2");
+        d2.start();
     }
 }
