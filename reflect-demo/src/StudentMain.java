efe09d9c903c531db2d3a144f683c168e37c466c
zxt_z <zxt_zel_npl@163.com>
2018-05-15 23:08:55 +0800

    finish the reflect

--- reflect-demo/src/StudentMain.java
+++ reflect-demo/src/StudentMain.java
@@ -1,5 +1,6 @@
 import fanshe.*;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 
 /*
  * 通过Class对象可以获取某个类中的：构造方法、成员变量、成员方法；并访问成员；
@@ -50,6 +51,56 @@ public class StudentMain {
         //调用构造方法
         con.setAccessible(true);//暴力访问(忽略掉访问修饰符)
         obj = con.newInstance('男');
+
+
+
+
+        /*
+         * 获取成员变量并调用：
+         *
+         * 1.批量的
+         * 		1).Field[] getFields():获取所有的"公有字段"
+         * 		2).Field[] getDeclaredFields():获取所有字段，包括：私有、受保护、默认、公有；
+         * 2.获取单个的：
+         * 		1).public Field getField(String fieldName):获取某个"公有的"字段；
+         * 		2).public Field getDeclaredField(String fieldName):获取某个字段(可以是私有的)
+         *
+         * 	 设置字段的值：
+         * 		Field --> public void set(Object obj,Object value):
+         * 					参数说明：
+         * 					1.obj:要设置的字段所在的对象；
+         * 					2.value:要为字段设置的值；
+         *
+         */
+
+        //3.获取字段
+        System.out.println("************获取所有公有的字段********************");
+        Field[] fieldsArray = clazz.getFields();
+        for(Field f:fieldsArray){
+            System.out.println(f);
+        }
+
+        System.out.println("************获取所有的字段(包括私有、受保护、默认的)********************");
+        fieldsArray = clazz.getDeclaredFields();
+        for(Field f:fieldsArray){
+            System.out.println(f);
+        }
+
+        System.out.println("*************获取公有字段**并调用***********************************");
+        Field f = clazz.getField("name");
+        System.out.println(f);
+        //获取一个对象
+        obj = clazz.getConstructor().newInstance();
+        f.set(obj,"刘德华");
+        stu = (Student)obj;
+        System.out.println("验证姓名："+stu.name);
+
+        System.out.println("**************获取私有字段****并调用********************************");
+        f=clazz.getDeclaredField("phoneNum");
+        System.out.println(f);
+        f.setAccessible(true);
+        f.set(obj,"18888889999");
+        System.out.println("验证电话："+stu);
 
     }
 }
--- reflect-demo/src/fanshe/Student.java
+++ reflect-demo/src/fanshe/Student.java
@@ -35,5 +35,13 @@ public class Student {
 
 
     //**********字段*************//
-    public
+    public String name;
+    protected int age;
+    char sex;
+    private String phoneNum;
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", age=" + age + ", sex=" + sex + ", phoneNum=" + phoneNum +"]";
+    }
 }
