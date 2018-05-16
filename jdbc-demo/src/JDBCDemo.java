1f09a6ae7ef4acad61535be91df1cf0cbb92bab9
zxt_z <zxt_zel_npl@163.com>
2018-05-16 23:05:51 +0800

    finish the reflect

--- jdbc-demo/src/JDBCDemo.java
+++ jdbc-demo/src/JDBCDemo.java
@@ -4,21 +4,21 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class JDBCDemo {
-    public static final String URL = "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true";
+    public static final String URL = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC&useSSL=false";
     public static final String USER = "root";
-    public static final String PASSWORD = "vcl111111";
+    public static final String PASSWORD = "96321";
 
     public static void main(String[] args) throws Exception {
         //1.加载驱动程序
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
         //2. 获得数据库连接
         Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
         //3.操作数据库，实现增删改查
         Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM people");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM person");
         //如果有数据，rs.next()返回true
         while(rs.next()){
-            System.out.println(rs.getString("user_name")+" 年龄："+rs.getInt("age"));
+            System.out.println(rs.getString("user_name")+" 年龄："+rs.getInt("id"));
         }
     }
 }
